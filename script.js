71be9fc7b4939d66428b848ff57f27affe722d45
Andy Gorbould <gorbould@mail.com>
2022-05-16 15:48:33 +0200

    changed multipliter amount, title score display, fixed decimals

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
+
+
+
 const cookie = document.getElementById("cookie");
 
 // init
-let countNumb = 0;
-
-
-let i = 1;
+let clicks = 0;
+let totalCount = 0;
+let multiplier = 1;
 
 
 // multiplier One
 multiplierOne.addEventListener("click", function () {
-    i++;
+    multiplier = multiplier * 1.2;
 })
 
 // increment
 cookie.addEventListener("click", function () {
-    countNumb++;
-    counter.innerHTML = `Counter: ${countNumb * i}`;
+    clicks++;
+    totalCount = clicks * multiplier;
+    counter.innerHTML = `Counter: ${totalCount.toFixed(3)}`;
 
 });
+
+
+
+
+
+
+
+// change title every 5 seconds
+setInterval (function titleUpdater() {
+document.title = `${totalCount.toFixed(3)} Cookie Clicker Score`;
+}, 5000);
+
+
+
+
+
+/// i = multiplier
+/// countNumb = clicks
+/// totalCount = clicks * multiplier
