b0a27c81285c48a1f76381b92a70060cb79c5f58
Andy Gorbould <103992327+AndyGorbould@users.noreply.github.com>
2022-05-16 13:54:28 +0200

    Initial upload of Cookie-Cutter task

---
+++ script.js
@@ -0,0 +1,22 @@
+let cookie = document.getElementById("cookie");
+
+// init
+let countNumb = 0;
+
+
+let i = 1;
+
+
+// multiplier One
+multiplierOne.addEventListener("click", function() {
+    
+    i + i;
+})
+
+
+// increment
+cookie.addEventListener("click", function() {
+    
+    counter.innerHTML = `Counter: ${countNumb + i}`;
+    countNumb++;
+    console.log("clicked") });
