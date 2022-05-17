8a305856ae285f1f2d2d5319201da2e6665aa23a
Andy Gorbould <gorbould@mail.com>
2022-05-17 11:55:01 +0200

    working multiplier thanks to Dimi :D

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-
-
-
 const cookie = document.getElementById("cookie");
 
 multiAmount = document.getElementById("multiAmount");
+
 
 // init
 let clicks = 0;
 let totalCount = 0;
 let multiplier = 1;
+let autoClick = 0;
 
 
 // multiplier One
 multiplierOne.addEventListener("click", function () {
+    // alert(totalCount);
+
+    if (totalCount >= 20) {
+        // alert(multiCost);
+        multiCost = 20;
+        totalCount = totalCount - multiCost;
+        counter.innerHTML = `Counter: ${totalCount.toFixed(0)}`;
+
+        }
+        
     multiplier = multiplier + 1.2;
     // show multiplier amt
     multiAmount.innerHTML = `${multiplier.toFixed(1)} x`;
@@ -20,23 +29,32 @@ multiplierOne.addEventListener("click", function () {
 
 // increment
 cookie.addEventListener("click", function () {
-    clicks++;
-    totalCount = clicks * multiplier;
-    counter.innerHTML = `Counter: ${totalCount.toFixed(1)}`;
+    clicks = ++totalCount;
+    totalCount = totalCount + multiplier;
+
 
 });
 
 
+// display score TOTAL
+function scoreDisplay() {
+
+    // change title every 1 seconds
+    setInterval(function titleUpdater() {
+        counter.innerHTML = `Counter: ${totalCount.toFixed(0)}`;
+        document.title = `${totalCount.toFixed(0)} Cookie Clicker Score`;
+    }, 100);
+}
+
+scoreDisplay();
 
 
 
 
 
 
-// change title every 5 seconds
-setInterval (function titleUpdater() {
-document.title = `${totalCount.toFixed(1)} Cookie Clicker Score`;
-}, 5000);
+
+
 
 
 
