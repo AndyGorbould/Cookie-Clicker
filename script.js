18aef826488adcf7a360ae6397b957d88d5b2b96
Andy Gorbould <gorbould@mail.com>
2022-05-17 20:29:10 +0200

    multiplier disable fixed *DO NOT CHANGE!*

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
+// elements from document
 const cookie = document.getElementById("cookie");
+const multiplierOne = document.getElementById("multiplierOne");
 
-multiAmount = document.getElementById("multiAmount");
-
-// init
+// init values
 let clicks = 0;
 let totalCount = 0;
-let multiplier = 0;
+let multiplier = 1;
 let autoClick = 0;
+let multiCost = 20;
 
-// multiplier One
+// gameplay loop (runs all functions every 0.1s)
+function gameplayLoop() {
+
+    setInterval(function () {
+        writeAll();
+        multiEnable()
+
+    }, 100);
+
+}
+
+gameplayLoop();
+
+// multiplier One buy
 multiplierOne.addEventListener("click", function () {
-    // alert(totalCount);
+    // calculate new total count
+    totalCount = totalCount - multiCost;
+    // increase click values
+    multiplier = multiplier + 0.2;
+    // increase multi cost
+    multiCost = multiCost * 1.5;
+    // next cost
+    console.log(`Next multiplier cost: ${multiCost}`);
+    });
 
-    if (totalCount >= 20) {
-        multiplierOne.setAttribute("enabled", "enabled");
-        multiCost = 20;
-        totalCount = totalCount - multiCost;
-
-    }
-    else {
-        multiplierOne.setAttribute("disabled", "disabled");
-    }
-
-    multiplier = multiplier + 1.2;
-    // show multiplier amt
-    multiAmount.innerHTML = `${multiplier.toFixed(1)} x`;
-})
-
-// increment
+// clicks
 cookie.addEventListener("click", function () {
-    clicks = clicks * multiplier;
-    clicks = ++totalCount;
-    // totalCount = totalCount + multiplier;
+    totalCount = totalCount + multiplier;
 });
 
 // display score TOTAL
-function scoreDisplay() {
-
-    // change title every 1 seconds
-    setInterval(function titleUpdater() {
-        counter.innerHTML = `Counter: ${totalCount.toFixed(1)}`;
-        document.title = `${totalCount.toFixed(1)} Cookie Clicker Score`;
-    }, 100);
+function writeAll() {
+    counter.innerHTML = `Counter: ${totalCount.toFixed(1)}`;
+    document.title = `${totalCount.toFixed(1)} Cookie Clicker Score`;
+    multiAmount.innerHTML = `${multiplier.toFixed(1)} x`;
 }
 
-scoreDisplay();
+// multi enable/disable
+function multiEnable() {
+    // multiplierOne.disabled = true;     ///////  need to disable button on pageload
+    if (multiCost <= totalCount) {
+        multiplierOne.disabled = false;
+    }
+    else {
+        multiplierOne.disabled = true;
+    }
+}
