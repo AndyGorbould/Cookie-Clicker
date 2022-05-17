df29af5201df8c9ec395fd9c962a18525c6987e5
Andy Gorbould <gorbould@mail.com>
2022-05-17 13:38:55 +0200

    added enable/disable multiplier button

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ const cookie = document.getElementById("cookie");
 
 multiAmount = document.getElementById("multiAmount");
 
-
 // init
 let clicks = 0;
 let totalCount = 0;
-let multiplier = 1;
+let multiplier = 0;
 let autoClick = 0;
-
 
 // multiplier One
 multiplierOne.addEventListener("click", function () {
     // alert(totalCount);
 
     if (totalCount >= 20) {
-        // alert(multiCost);
+        multiplierOne.setAttribute("enabled", "enabled");
         multiCost = 20;
         totalCount = totalCount - multiCost;
-        counter.innerHTML = `Counter: ${totalCount.toFixed(0)}`;
 
-        }
-        
+    }
+    else {
+        multiplierOne.setAttribute("disabled", "disabled");
+    }
+
     multiplier = multiplier + 1.2;
     // show multiplier amt
     multiAmount.innerHTML = `${multiplier.toFixed(1)} x`;
@@ -29,40 +29,19 @@ multiplierOne.addEventListener("click", function () {
 
 // increment
 cookie.addEventListener("click", function () {
+    clicks = clicks * multiplier;
     clicks = ++totalCount;
-    totalCount = totalCount + multiplier;
-
-
+    // totalCount = totalCount + multiplier;
 });
-
 
 // display score TOTAL
 function scoreDisplay() {
 
     // change title every 1 seconds
     setInterval(function titleUpdater() {
-        counter.innerHTML = `Counter: ${totalCount.toFixed(0)}`;
-        document.title = `${totalCount.toFixed(0)} Cookie Clicker Score`;
+        counter.innerHTML = `Counter: ${totalCount.toFixed(1)}`;
+        document.title = `${totalCount.toFixed(1)} Cookie Clicker Score`;
     }, 100);
 }
 
 scoreDisplay();
-
-
-
-
-
-
-
-
-
-
-
-
-
-/// i = multiplier
-/// countNumb = clicks
-/// totalCount = clicks * multiplier
-
-/// mOnePrice = 15;
-/// mOnePrice = mOnePrice + 1.4;
